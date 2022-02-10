484c69e4a108dd2144f0f62f1c242b8b6be798f7
Denys Kochergin <dangerspace7@gmail.com>
2022-02-10 22:35:33 +0200

    добавил изменения

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,18 @@
 const inputEl = document.querySelector('input');
 
-inputEl.addEventListener('blur', event => {
-  if (event.currentTarget.value.length >= inputEl.dataset.length) {
-    inputEl.classList.add('valid');
-    inputEl.classList.remove('invalid');
-  } else {
-    inputEl.classList.add('invalid');
-    inputEl.classList.remove('valid');
+inputEl.addEventListener('blur', onInputValidation);
+
+function onInputValidation(event) {
+  const valueLength = event.currentTarget.value.length;
+  const requiredLength = event.currentTarget.dataset.length;
+
+  this.classList.add('invalid');
+  this.classList.remove('valid');
+
+  if (requiredLength === inputEl.dataset,
+    valueLength == requiredLength) {
+    this.classList.add('valid');
+    this.classList.remove('invalid');
   }
-});
+}
+
--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
-const textSpan = document.querySelector(`#text`)
-const input = document.querySelector(`#font-size-control`)
-console.log (input)
-console.log(textSpan.textContent)
-input.addEventListener (`change`,function(){
-    textSpan.style.fontSize = input.value + "px"
-});
+const input = document.querySelector('#font-size-control');
+const textSpan = document.querySelector('#text');
+
+input.addEventListener(
+  'input',
+  event => (textSpan.style.fontSize = `${event.currentTarget.value}px`),
+);
